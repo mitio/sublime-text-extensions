e23900ff26c7c0bd0c8e44856a72ef07956b4b58
Dimitar Dimitrov <wireman@gmail.com>
2011-11-18 18:06:49 +0200

    Fix the install script
    Fix Bourne shell compatibility
    Fix pathnames with spaces

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 unamestr=`uname`
-if [ "$unamestr" == 'Linux' ]; then
-	SUBLIME_SHARED_FOLDER="~/.config/sublime-text-2"
-elif [ "$unamestr" == 'Darwin' ]; then
-	SUBLIME_SHARED_FOLDER="~/Library/Application Support/Sublime Text 2"
+if [ "$unamestr" = 'Linux' ]; then
+	SUBLIME_SHARED_FOLDER="~/.config/sublime-text-2/Packages"
+elif [ "$unamestr" = 'Darwin' ]; then
+	SUBLIME_SHARED_FOLDER="$HOME/Library/Application Support/Sublime Text 2/"
 else
 	echo 'Unsupported platform!'
 	echo 'Please inspect the install.sh script and manually set the SUBLIME_SHARED_FOLDER variable.'
 	exit 1
 fi
 
-echo "Installing to: ${SUBLIME_SHARED_FOLDER}"
+echo "Installing the following packages to: ${SUBLIME_SHARED_FOLDER}"
 
 INSTALL_SOURCE=`dirname "$0"`
 INSTALL_SOURCE="${INSTALL_SOURCE}/install/Packages"
@@ -19,7 +19,8 @@ INSTALL_SOURCE="${INSTALL_SOURCE}/install/Packages"
 ls $INSTALL_SOURCE | while read package
 do
 	echo "  ${package}"
-	cp -rf "${INSTALL_SOURCE}/${package}" "${SUBLIME_SHARED_FOLDER}"
 done
+
+cp -R ${INSTALL_SOURCE} '${SUBLIME_SHARED_FOLDER}'
 
 echo "Done."
